671de5da0242cbdf204e7191ed893bdc223a452f
BuBuS <bubus258@gmail.com>
2021-02-27 23:42:18 +0100

    added basic error handling and discord intents

--- source/bot/main.py
+++ source/bot/main.py
@@ -1,6 +1,8 @@
 from discord.ext.commands import Bot as Base
+from discord.ext.commands import CommandNotFound
+from discord import Intents
+from discord import Embed
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
 # Prefix for chat commands
 PREFIX = "."
 # User ID from Discord
@@ -15,7 +17,11 @@ class Bot(Base):
         self.guild = None
         self.scheduler = AsyncIOScheduler()
 
-        super().__init__(command_prefix=PREFIX, owner_id=OWNER_ID)
+        super().__init__(
+            command_prefix=PREFIX, 
+            owner_id=OWNER_ID, 
+            intents=Intents.all(),
+        )
     
     def run(self):
         with open("./config/TOKEN.txt", "r", encoding="utf-8") as token_file:
@@ -37,6 +43,21 @@ class Bot(Base):
             print("Bot is ready.")
         else:
             print("Bot reconnected.")
+
+    #////////// Error Handling ///////////
+    async def on_error(self, error, *args, **kwargs):
+        if error == "on_command_error":
+            await args[0].send("A command error occurred.")
+        raise
+    
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, CommandNotFound):
+            await ctx.send("Wrong command")
+        elif hasattr(exc, ["original"]):
+            raise exc.original
+        else:
+            raise exc
+    #/////////////////////////////////////
 
     async def on_message(self, message):
         pass
